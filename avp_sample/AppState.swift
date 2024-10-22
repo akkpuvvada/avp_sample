6e3433533a0723badcdbb19ab2c368caacacfa07
akshay <akk.puvvada@gmail.com>
2024-10-22 09:26:35 -0500

    Adding the starting scenario phase for the app

--- avp_sample/AppState.swift
+++ avp_sample/AppState.swift
@@ -12,6 +12,7 @@ import Spatial
 public enum AppPhase: CaseIterable, Codable, Identifiable, Sendable {
     case initialize // to show instructions and welcome screen
     case configure // select scene and trafffic
+    case startscenario // roadcross scenario will be started
     
     public var id: Self { self }
 }
